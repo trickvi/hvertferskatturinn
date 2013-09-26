d975e2f603eab12c3fb0b2e4f3ae5e4bd07d6832
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2013-09-26 18:53:20 +0000

    Added Icelandic icons

---
+++ js/icelandic.icons.js
@@ -0,0 +1,26 @@
+OpenSpending.Icons.Icelandic = {
+    '01': 'icelandic/skjaldarmerki.svg',
+
+    '03': 'icelandic/log-og-regla.svg',
+    '03.1': 'icelandic/logreglan.svg',
+    '03.1.0': 'icelandic/logreglan.svg',
+    '03.6': 'icelandic/log-og-regla-umsjon.svg',
+    '03.6.0': 'icelandic/log-og-regla-umsjon.svg',
+
+    '04.3': 'icelandic/idnadur.svg',
+    '04.2': 'icelandic/sjor-og-land.svg',
+    '04.8.2': 'icelandic/sjor-og-land.svg',
+    '04.8.4': 'icelandic/idnadur.svg',
+
+    '07': 'ambulance.svg',
+    '07.4': 'health.svg',
+
+    '08.4': 'icelandic/christianity.svg',
+    '08.4.0': 'icelandic/christianity.svg',
+
+    '10.1.1': 'hospital.svg'
+};
+
+OpenSpending.Icons.Cofog = $.extend(OpenSpending.Icons.Cofog,
+				    OpenSpending.Icons.Icelandic)
+				    
